bf0703791a730b6cd00f6c5b55465e82cf25de61
user.name like:"Luv <bhardwaj.baljit@gmail.com>
2025-01-25 15:26:49 +0530

    removing export

--- index.js
+++ index.js
@@ -72,9 +72,6 @@ app.post("/login", async (req, res) => {
     }
 });
 
-// Export the app for Vercel
-module.exports = app;
-
 // Start the server only if not in a Vercel environment
 if (process.env.VERCEL !== "1") {
     app.listen(PORT, () => {
