4ef6c833340f9abc3c47995d64f89f287ba99b3c
user.name like:"Luv <bhardwaj.baljit@gmail.com>
2025-01-25 21:40:05 +0530

    added the api along with js file

--- public/javascripts/weather.js
+++ public/javascripts/weather.js
@@ -1,18 +1,30 @@
-const button = document.getElementById("search")
-const input = document.getElementById("cityinput")
-const country = document.getElementById("country")
-const localtime = document.getElementById("localtime")
+const apikey = "ec5b58a0f5b045d1b7e123216250601"
+const cityname = document.getElementById("cityinput")
+const search = document.getElementById("search")
+const weather = document.getElementById("weather")
 const temp = document.getElementById("temp")
-const apiKey = "ec5b58a0f5b045d1b7e123216250601"
-const getdata = async() =>{
-    let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${input.value}&aqi=yes`
-    let data = await fetch(url)
+const image = document.getElementById("wimg")
+const fetchdata = async() =>{
+    let data = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${cityname.value}&aqi=yes`)
     let response = await data.json()
-    country.innerText = `${response.location.country} , ${response.location.region} - ${response.location.name}`
-    localtime.innerText = `${response.location.localtime}`
-    temp.innerText = `Temperature is ${response.current.temp_c}°C`
-    console.log(response)
+    weather.innerText = `${response.current.condition.text}`
+    if(response.current.condition.text=="Partly cloudy" || response.current.condition.text=="cloudy"){
+        image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxuyj9KaQPjFFzJ-HCiHKjTLk-HX3d6rOxUg&s"
+    }
+    else if(response.current.condition.text=="Overcast"){
+        image.src = "https://cdn-icons-png.flaticon.com/256/5546/5546134.png"
+    }
+    else if(response.current.condition.text=="Mist"){
+        image.src = "https://static.thenounproject.com/png/3244281-200.png"
+    }
+    else if(response.current.condition.text=="Clear"){
+        image.src = "https://cdn-icons-png.flaticon.com/256/3222/3222807.png"
+    }
+    else{
+        image.src = "https://img.icons8.com/color/96/000000/sun.png"
+    }
+    temp.innerText = `${response.current.temp_c}°C`
 }
-button.addEventListener("click",()=>{
-    getdata()
+search.addEventListener("click",()=>{
+    fetchdata()
 })
